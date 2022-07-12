9cb8d5e360e19f3d98371edc38a04ebd24b7526b
Yunhao-Luo <86722488+Yunhao-Luo@users.noreply.github.com>
2022-07-12 13:49:02 -0700

    updated azure_test

--- azure_test.py
+++ azure_test.py
@@ -15,35 +15,8 @@ try:
  
    # Create the BlobServiceClient object which will be used to create a container client
    blob_service_client = BlobServiceClient.from_connection_string(connect_str)
- 
-   # Create a unique name for the container
-   container_name = str(uuid.uuid4())
-   container_name = "testingazure"
- 
-   # Create the container
-   container_client = blob_service_client.create_container(container_name)
- 
-   # Create a local directory to hold blob data
-   local_path = "./data_blob_test"
-   os.mkdir(local_path)
- 
-   # Create a file in the local data directory to upload and download
-   local_file_name = str(uuid.uuid4()) + ".txt"
-   upload_file_path = os.path.join(local_path, local_file_name)
- 
-   # Write text to the file
-   file = open(upload_file_path, 'w')
-   file.write("Hello, World!")
-   file.close()
- 
-   # Create a blob client using the local file name as the name for the blob
-   blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
- 
-   print("\nUploading to Azure Storage as blob:\n\t" + local_file_name)
- 
-   # Upload the created file
-   with open(upload_file_path, "rb") as data:
-       blob_client.upload_blob(data)
+
+   container_client = blob_service_client.get_container_client("downloadtest")
  
    print("\nListing blobs...")
  
@@ -54,14 +27,19 @@ try:
  
    # Download the blob to a local file
    # Add 'DOWNLOAD' before the .txt extension so you can see both files in the data directory
-   #local_path = "Users\WorldViz.VIZBOX-03\Desktop\data_display_GUI-main"
-   #local_file_name = "test.txt"
-   download_file_path = os.path.join(local_path, str.replace(local_file_name ,'.txt', 'DOWNLOAD.txt'))
-   blob_client = blob_service_client.get_container_client(container= container_name)
+   local_path = "./data_blob_test"
+   os.mkdir(local_path)
+   download_file_path = os.path.join(local_path)
+   blob_client = blob_service_client.get_container_client(container= "downloadtest")
    print("\nDownloading blob to \n\t" + download_file_path)
+
+   # To do: create files with names in blob_list and use download_blob().readall() to write contents to each file.
+   # Modify the code below to download all files.
+   # To do: for blob in blob_list, create a file with blob name & write contents from download_blob into the file.
  
    with open(download_file_path, "wb") as download_file:
-       download_file.write(blob_client.download_blob(blob.name).readall())
+        for blob in blob_list:
+            download_file.write(blob_client.download_blob(blob.name).readall())
  
  
  
